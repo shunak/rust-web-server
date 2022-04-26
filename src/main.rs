1ba4b505b22051d4ed93cbce509b47d188c7d185
shunak <shuna966@gmail.com>
2022-04-26 17:20:41 +0900

    LoadTest for multi thread server

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,9 @@ use std::net::TcpListener;
 use std::net::TcpStream;
 use std::io::prelude::*;
 use std::fs::File;
+use std::thread;
+use std::time::Duration;
+
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -19,8 +22,12 @@ fn handle_connection(mut stream: TcpStream){
     stream.read(&mut buffer).unwrap();
 
     let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
 
     let (status_line, filename) = if buffer.starts_with(get){
+        ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
+    }else if buffer.starts_with(sleep){
+        thread::sleep(Duration::from_secs(5));
         ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
     } else {
         ("HTTP/1.1 400 NOT FOUND\r\n\r\n", "404.html")
